8264841d62d5784575ea5c8952716c5b2d2e9f9a
marcosurfacepro <marcocosta55@gmail.com>
2019-09-02 10:24:08 -0400

    working on the base class.

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -6,6 +6,7 @@ class GeneticAlgorithm(object):
 
     def __init__(self, pop_size=10, chrom_len=10, g_size=11):
         """
+        Initializes our first generation population of chromosomes that all have randomly generated integer type genes.
         :param pop_size: the size of the entire population.
         :param chrom_len: how many genes are in a chromosome. (eg strand of DNA)
         :param g_size: how many possibilities are held within each gene. (eg. for human DNA this number would be 3)
@@ -15,7 +16,8 @@ class GeneticAlgorithm(object):
         self._GENE_SIZE = g_size
         self._FINAL_GENERATION = 1
 
-        # setting up an initial population.
+        # initializing our class variable 'population' to be a DataFrame of randomly generated chromosomes
+        # and NaN fitness values.
         self.population = pd.DataFrame(columns=['chromosome', 'fitness'], index=range(0,pop_size))
         for i in range(pop_size):
             temp_gene = []
@@ -24,11 +26,20 @@ class GeneticAlgorithm(object):
             self.population['chromosome'][i] = temp_gene
 
     def __str__(self):
+        """
+        Makes it possible to print the entire GeneticAlgorithm object.
+        :return: the entire population DataFrame cast as a string
+        """
         return str(self.population)
 
     def _fitness(self):
+        """
+        Simple placeholder fitness function that determines fitness based on the value of the sum of all genes.
+        This function is designed to be overridden by child classes as it really has no practical use.
+        """
         for i in range(self._POPULATION_SIZE):
             self.population['fitness'][i] = sum(self.population['chromosome'][i])
+
         self.population = self.population.sort_values('fitness', ascending=False)
         self.population = self.population.reset_index(drop=True)
 
