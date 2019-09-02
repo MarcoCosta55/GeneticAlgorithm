06c7cb264dd40086afa6e909a69ff608517fb9ef
marcosurfacepro <marcocosta55@gmail.com>
2019-09-01 21:42:58 -0400

    initial updates with MusicGA

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,16 +1,34 @@
+from random import randrange
+import pandas as pd
+
+
 class GeneticAlgorithm(object):
 
-    def __init__(self, pop_size=100, chrom_size=16):
+    def __init__(self, pop_size=10, chrom_len=10, g_size=11):
+        """
+        :param pop_size:
+        :param chrom_len:
+        :param g_size:
+        """
         self._POPULATION_SIZE = pop_size
-        self._CHROMOSOME_SIZE = chrom_size
-        self._FINAL_GENERATION = 1 # class is initialized with only one possible generation until evolve is called.
-        self._initial_pop()
+        self._CHROMOSOME_LENGTH = chrom_len  # how many genes are in each chromosomes.
+        self._GENE_SIZE = g_size  # this controls how many possible variations of each gene there are.
+        self._FINAL_GENERATION = 1  # class is initialized with only one possible generation until evolve is called.
 
-    def _initial_pop(self):
-        pass
+        # setting up an initial population.
+        self.population = pd.DataFrame(columns=['chromosome', 'fitness'], index=range(0,pop_size))
+        for i in range(pop_size):
+            temp_gene = []
+            for c in range(chrom_len):
+                temp_gene.append(randrange(g_size))
+            self.population['chromosome'][i] = temp_gene
+        self._fitness()
+
+        print(self.population)
 
     def _fitness(self):
-        pass
+        for i in range(self._POPULATION_SIZE):
+            self.population['fitness'][i] = sum(self.population['chromosome'][i])
 
     def evolve(self, gen):
         self._FINAL_GENERATION = gen
@@ -31,8 +49,11 @@ class GeneticAlgorithm(object):
     def get_pop_size(self):
         return self._POPULATION_SIZE
 
-    def get_chrom_size(self):
-        return self._CHROMOSOME_SIZE
+    def get_chrom_length(self):
+        return self._CHROMOSOME_LENGTH
 
     def get_final_gen(self):
         return self._FINAL_GENERATION
+
+    def get_gene_size(self):
+        return self._GENE_SIZE
--- test_GA.py
+++ test_GA.py
@@ -1,5 +1,4 @@
 from GeneticAlgorithm import GeneticAlgorithm as ga
 
 g1 = ga()
-g1.evolve(1000)
-print(g1.get_chrom_size())
+
