206d4f62b4f38897910671d500d34f1edd244214
MarcoLaptop <marcocosta55@gmail.com>
2019-09-10 22:40:53 -0400

    Starting a new child class that evolves to a inputted string.

--- test_StringGA.py
+++ test_StringGA.py
@@ -2,7 +2,7 @@ from StringGA import StringGA
 
 g1 = StringGA()
 g1.evolve()
-print(list(g1._ascii_dict.keys())[list(g1._ascii_dict.values()).index('G')])
+print(list(g1._ascii_dict.values()).index('G'))
 print((len(g1._ascii_dict)))
 for i in g1._ascii_dict:
     if g1._ascii_dict[i] == 'G':
