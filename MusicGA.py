ac0484428e1f6c24efca11a79eb01863a7b9f342
marcosurfacepro <marcocosta55@gmail.com>
2019-11-17 21:11:39 -0500

    working on implementing jump fitness for MusicGA.

--- MusicGA.py
+++ MusicGA.py
@@ -9,7 +9,7 @@ from GeneticAlgorithm import GeneticAlgorithm
 
 class MusicGA(GeneticAlgorithm):
 
-    def __init__(self, pop_size=10, chrom_len=10):
+    def __init__(self, pop_size=10, chrom_len=10, g_size=45):
 
         # This dictionary provides the basic decoding mechanism for each gene. Each gene that is generated corresponds
         # to a key in the dictionary that references a tuple. The tuple will provide a more verbose definition of the
@@ -26,24 +26,32 @@ class MusicGA(GeneticAlgorithm):
                            40: ('G#', 8, 1), 41: ('G#', 8, 2), 42: ('G#', 8, 4), 43: ('G#', 8, 8), 44: ('G#', 8, 16)}
 
         # G_SIZE directly relates to the _NOTE_DICT size and should not be changed unless the note dictionary changes.
-        GeneticAlgorithm.__init__(self, pop_size, chrom_len, g_size=45)
+        GeneticAlgorithm.__init__(self, pop_size, chrom_len, g_size)
+
+    def __str__(self) -> 'string':
+        """
+        This will print the first (most fit) chromosome as a comma separated string that is readable in english.
+        :return: string type object.
+        """
+        music_string = ''
+        for i in range(self._CHROMOSOME_LENGTH):
+            music_string += self._NOTE_DICT[self.population['chromosome'][1][i]][0] + \
+                            str(self._NOTE_DICT[self.population['chromosome'][1][i]][2]) + ', '
+
+        return music_string
 
     def _fitness(self):
         for i in range(self._POPULATION_SIZE):
-            jump_fitness = self._jump_fitness(self._NOTE_DICT[i])
+            jump_fitness = self._jump_fitness(self.population['chromosome'][i])
             self.population['fitness'][i] = jump_fitness
 
     def _jump_fitness(self, curr_chrom):
         jump_total = 0
         i = 0
 
-        while i < self._GENE_SIZE - 1:
-            if abs(self._NOTE_DICT[curr_chrom][1] - self._NOTE_DICT[curr_chrom + 1][1]) <= 1:
-                jump_total += 5
-            elif abs(self._NOTE_DICT[curr_chrom][1] - self._NOTE_DICT[curr_chrom + 1][1]) <= 2:
+        while i < self._CHROMOSOME_LENGTH - 1:
+            if abs(self._NOTE_DICT[curr_chrom[i]][1] - self._NOTE_DICT[curr_chrom[i+1]][1]) <= 3:
                 jump_total += 10
-            elif abs(self._NOTE_DICT[curr_chrom][1] - self._NOTE_DICT[curr_chrom + 1][1]) <= 3:
-                jump_total += 5
             else:
                 jump_total += 0
             i += 1
